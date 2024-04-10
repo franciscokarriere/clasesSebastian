faf09ae58373ce68794159661ae4766795a37cf0
Francisco Karriere <francisco.karriere@gmail.com>
2024-04-09 21:29:42 -0500

    inicializado de clase

--- chaos/ChaosGame.py
+++ chaos/ChaosGame.py
@@ -2,3 +2,17 @@ import tkinter as tk
 import random
 from Point import Point
 
+
+# clase ChaosGame numero de puntos para plotear 50000 por default, 
+# canvas de 600 x 520
+# 
+class ChaosGame:
+    def  __init__(self,master, width=600, height=520, num_points=50000):
+        self.master = master
+        self.height=height
+        self.width=width
+        self.num_points=num_points
+        self.canvas=tk.Canvas(master,width=width,height=height)
+        self.canvas.pack()
+
+        self.master.title("The Chaos Game!")
