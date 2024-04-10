28adf7e4c768e1160e5de8f260a252cce24641c1
stamayoz <stamayo1011@gmail.com>
2024-04-09 22:51:42 -0400

    Clase Chaos

--- chaos/ChaosGame.py
+++ chaos/ChaosGame.py
@@ -5,14 +5,38 @@ from Point import Point
 
 # clase ChaosGame numero de puntos para plotear 50000 por default, 
 # canvas de 600 x 520
-# 
+
+#Clase del juego
 class ChaosGame:
     def  __init__(self,master, width=600, height=520, num_points=50000):
         self.master = master
         self.height=height
         self.width=width
         self.num_points=num_points
-        self.canvas=tk.Canvas(master,width=width,height=height)
+
+        #Definicion global de listas vacias para los vertices y midpoints
+        self.vertices = []
+        self.midpoints = []
+
+        #configuracion de canva
+        self.canvas=tk.Canvas(master, width=width, height=height)
         self.canvas.pack()
+        self.canvas.config(bg="white")
 
         self.master.title("The Chaos Game!")
+
+
+        #Llamado a funciones
+        self.generate_vertices()
+        self.plot_vertices()
+        self.generate_midpoints()
+    
+    #Funcion para generar vertices
+    # v1, v2, v3 ← three vertices of an equilateral triangle
+    def generate_vertices(self):
+        self.vertices.append(Point(100,400))
+        self.vertices.append(Point(300,100))
+        self.vertices.append(Point(500,400))
+        
+    def plot_vertices(self):
+        
